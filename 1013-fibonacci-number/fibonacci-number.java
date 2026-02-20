d484edd2cabbc886502dd5f59bc7f1044ba48b12
Bandaru Akhil Kumar Reddy <153007482+Akhilreddy175@users.noreply.github.com>
2026-02-20 17:31:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (14.18%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,10 +1,24 @@
 class Solution {
     public int fib(int n) {
-        
-        if(n <= 1 ){
+
+        if(n <=1){
             return n;
         }
 
-        return fib(n-1) +fib(n-2);
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        int pre = 0;
+        int curr = 1;
+
+        for(int i=2;i<=n;i++){
+            int next = pre + curr;
+            pre = curr;
+            curr = next;
+
+            map.put(i,curr);
+        }
+
+        return map.get(n);
+
     }
 }
