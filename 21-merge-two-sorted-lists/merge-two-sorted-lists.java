fa25cfdcd6923d1237e9abe2583799f1c7242fa0
Bandaru Akhil Kumar Reddy <153007482+Akhilreddy175@users.noreply.github.com>
2026-04-01 18:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (29.63%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -28,14 +28,12 @@ class Solution {
 
         if(list1 != null){
             sorted.next = list1;
-            list1 = list1.next;
-            sorted = sorted.next;
+            
         }
 
         if(list2 != null){
             sorted.next = list2;
-            list2 = list2.next;
-            sorted = sorted.next;
+           
         }
 
         return dummy.next;
