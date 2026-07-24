b03bf0b468598c2d1c926be04cdf5f41f9ec05a7
Bandaru Akhil Kumar Reddy <153007482+Akhilreddy175@users.noreply.github.com>
2026-07-24 12:26:22 +0530

    Time: 0 ms (100%), Space: 42.9 MB (68.26%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int strStr(String str1, String str2) {
+
+        // if(!str1.contains(str2)){
+        //     return -1;
+        // }
+
+        // if(str1.startsWith(str2)){
+        //     return 0;
+        // }
+        return  str1.indexOf(str2);
+    }
+}
