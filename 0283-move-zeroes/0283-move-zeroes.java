b48b4162bba42cd516b5e78f6fa65a66687106d8
Bandaru Akhil Kumar Reddy <153007482+Akhilreddy175@users.noreply.github.com>
2026-08-26 18:06:53 +0530

    Time: 2 ms (91.93%), Space: 47.7 MB (57.6%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] a) {
+        int j = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != 0) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                j++;
+            }
+        }
+    }
+}
